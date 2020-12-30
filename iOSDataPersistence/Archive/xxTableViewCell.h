f2521ca0ca1564464c252bb21dfb40337694ce53
ASSASSINYM <darlingiloveu@sina.com>
2020-12-30 14:41:45 +0800

    xxx

---
+++ iOSDataPersistence/Archive/xxTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  xxTableViewCell.h
+//  iOSDataPersistence
+//
+//  Created by ASSASSIN on 2020/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface xxTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
